949d362080d4dd3f172e6c64ee8fd84d3c17ec2f
kyledag500 <kyledag500@gmail.com>
2013-11-11 22:17:28 -0500

    Revert "Testing"
    This reverts commit 34016b43d0c5c252671d29915077e1d977fd6b8e.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,9 +4,5 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener{
-  
-  public void onEnable{
-    REGISTER THOSE EVENTS. I am just testing out github guys. If these things need to be removed, let me know. I dont think its a huge deal though. These take ten seconds to do.
-  }
 
 }
