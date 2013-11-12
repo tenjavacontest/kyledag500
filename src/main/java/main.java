9e9f45c106bbc51d3d5751070701ebb5c33de468
kyledag500 <kyledag500@gmail.com>
2013-11-11 22:18:38 -0500

    Still doing tests

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,13 @@
 package main.java;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener{
+	
+	public void onEnable(){
+		Bukkit.broadcastMessage("HELLO");
+	}
 
 }
