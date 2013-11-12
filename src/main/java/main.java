5d585eae038646b6f740ac1aff6f34ac055d03db
kyledag500 <kyledag500@gmail.com>
2013-11-11 22:11:54 -0500

    Changed Pom
    Please, someone make sure I am doing maven right. I have no idea WTF I
    am doing.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
 package main.java;
 
-public class main {
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener{
 
 }
